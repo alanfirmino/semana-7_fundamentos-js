541a39f3b9dcf4ac1f709158689555b109fb2917
alanfirmino <alan.l2@hotmail.com>
2022-03-18 21:12:28 -0300

    Curso de Fundamentos JS

--- type-string.js
+++ type-string.js
@@ -1,9 +1,13 @@
-const cifrao = '\u0024'
-const aMaiusculo = '\u0041'
-const tique = '\u2705'
-const hiragana = '\u3041'
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
 
-console.log(cifrao)
-console.log(aMaiusculo)
-console.log(tique)
-console.log(hiragana)
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
